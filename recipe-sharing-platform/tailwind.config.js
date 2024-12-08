fd216d706b764d0de07d8f9a64afecb6ca67624a
emeka bahati <emekabahati44@gmail.com>
2024-12-08 10:17:03 +0300

    new recipe sharing app

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Specify directories and files
+  theme: {
+    extend: {}, // Custom theme configurations can be added here later
+  },
+  plugins: [], // Add plugins if needed in the future
+};
